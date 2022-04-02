34538bf31931840259249bcec1fa106155b87197
sszy <1668653958@qq.com>
2022-04-02 12:37:45 +0800

    改

--- js/login.js
+++ js/login.js
@@ -26,7 +26,7 @@
         });
         // res && window.location.replace("../index.html");
 
-        res && (window.location.href = '../index.html');
+        res && (window.location.href = baseURL + 'index.html');
     };
 
     init();
